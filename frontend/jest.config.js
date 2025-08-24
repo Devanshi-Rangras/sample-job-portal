2247c21aee471d054744a1fb899a30bdd2a0d074
Devanshi Rangras <rangrasdevanshi@gmail.com>
2025-08-24 14:33:05 +0530

    Create jest.config.js

---
+++ frontend/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(axios)/)',  // transform axios ESM
+  ],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+};
