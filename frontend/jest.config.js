743ae1fe5a01d0ce378aac6413f7c49f11013865
Devanshi Rangras <rangrasdevanshi@gmail.com>
2025-08-24 19:19:19 +0530

    Update jest.config.js

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -11,4 +11,14 @@ module.exports = {
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
   },
   setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+
+  // Enable coverage
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!src/**/*.test.{js,jsx,ts,tsx}',
+    '!src/setupTests.{js,ts}',
+    '!src/**/index.{js,ts}',
+  ],
 };
